fe7d045be96ae17e050239b95d1feee26447922c
Alexson Almeida <as3904527@gmail.com>
2025-02-11 10:40:22 -0300

    adding navigation and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Landing } from "./Landing"
 import MainLayout from "./pages/components/Layout";
 import DocsPage from "./pages/DocsPage";
+import { CodeSmells } from "./pages/CommunErrors/CodeSmells";
+import { GoodPracticesPage } from "./pages/GoodPractices/GoodPracticesPage";
+import { BoxShadowComponents } from "./pages/ComponentsPages/BoxShadow";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
-          <Route element={<MainLayout />}>
-          <Route path="/introduction" element={<DocsPage />} />
+        <Route path="/" element={<Landing/>} />
+          <Route element={<MainLayout/>}>
+          <Route path="/introduction" element={<DocsPage/>}/>
+          <Route path="/code-smells" element={<CodeSmells/>}/>
+          <Route path="/good-practices" element={<GoodPracticesPage/>}/>
+          <Route path="/box-shadows-components" element={<BoxShadowComponents/>}/>
         </Route>
       </Routes>
     </Router>
--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,20 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from '../components/Sidebar';
 import Header from '../components/Header';
+import Footer from '@/components/landing/Footer';
 
-const MainLayout = () => {
+export default function MainLayout() {
   return (
     <div className="flex-col min-h-screen bg-gray-50">
       <Header/>
-      <div className="h-28 md:h-16" />
+      <div className="h-28 md:h-16"/>
       <div className="flex flex-1">
         <Sidebar className="bg-gray-50 hidden md:block"/>
         <main className="p-6 md:ml-56">
           <Outlet/>
         </main>
       </div>
+      <div className='flex flex-1'>
+        <div className="w-0 md:w-64"/>
+        <Footer/>
+      </div>
     </div>
   );
 };
-
-export default MainLayout;
